1c876578a3d22b38c883114bc1aa5be646f50f95
sofia garnero <sofigarnero@gmail.com>
2023-06-29 16:59:40 -0300

    tercer entrega!

---
+++ productos.js
@@ -0,0 +1,56 @@
+
+
+const productos = [{
+    id: 1,
+    nombre: "Sérum facial",
+    foto:"https://images.pexels.com/photos/7796740/pexels-photo-7796740.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    precio: 7500
+},
+{ 
+    id: 2,
+     nombre: "Sombras de ojos",
+     foto:"https://images.pexels.com/photos/4702570/pexels-photo-4702570.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+     url:"//www.pexels.com/es-es/foto/maquillaje-colores-sombra-de-ojos-productos-cosmeticos-4702570/",
+      precio: 5720 
+    },
+{ 
+    id: 3,
+     nombre: "Labial",
+     foto:" https://images.pexels.com/photos/90297/pexels-photo-90297.jpeg",
+     url:"https://www.pexels.com/es-es/foto/pintalabios-rojo-90297/",
+      precio: 2980 
+    },
+{ 
+    id: 4,
+     nombre: "Kit de skin care",
+     foto:"https://images.pexels.com/photos/3785147/pexels-photo-3785147.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+     url:"//www.pexels.com/es-es/foto/lay-flat-de-tratamiento-de-belleza-3785147/",
+      precio: 3580 
+    },
+{ 
+    id: 5,
+     nombre: "Iluminador",
+     foto:"https://images.pexels.com/photos/4938498/pexels-photo-4938498.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      precio: 3100
+     },
+{ 
+    id: 7,
+     nombre: "Polvo volatil ",
+     foto: "https://images.pexels.com/photos/1499516/pexels-photo-1499516.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      precio: 2580 
+    },
+{ 
+    id: 8,
+     nombre: "Mascarillas",
+     foto:"https://images.pexels.com/photos/4612123/pexels-photo-4612123.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", 
+     precio: 1000 
+    },
+{ 
+    id: 9,
+     nombre: "Brochas",
+     foto:"https://images.pexels.com/photos/1472098/pexels-photo-1472098.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+     
+      precio: 10000 
+    }
+
+]
